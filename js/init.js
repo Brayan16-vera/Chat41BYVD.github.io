24ebc9c9340c96b22f3336946f9e463512c6e8de
Brayan <87156215+SorekVan@users.noreply.github.com>
2021-07-08 20:36:09 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyDmovq7T_-Ym7iutIjJIN1ABI33usaMmJo",
-    authDomain: "autenfi41.firebaseapp.com",
-    projectId: "autenfi41",
-    storageBucket: "autenfi41.appspot.com",
-    messagingSenderId: "777550911881",
-    appId: "1:777550911881:web:205487691c96b111382958",
-    measurementId: "G-YHTXKL0597"
+   apiKey: "AIzaSyAvYkapMJGUFeBvmKEr_g0SrRsugqmYuKM",
+    authDomain: "autentic41.firebaseapp.com",
+    projectId: "autentic41",
+    storageBucket: "autentic41.appspot.com",
+    messagingSenderId: "858820619457",
+    appId: "1:858820619457:web:2ffa37c8ea17d13a097840",
+    measurementId: "G-VR74YY4R3V"
 });
